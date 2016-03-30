955f1f945b29f4fb3156ca90c9faf625f3fe108e
Antoine-T <antoine.thiebaud@devinci.fr>
2016-03-30 10:10:32 +0200

    girr
    alp

---
+++ src/IRTenor.java
@@ -0,0 +1,26 @@
+
+public enum IRTenor {
+	
+	_025yr(0.25),
+	_05yr(0.5),
+	_1yr(1.0),
+	_2yr(2.0),
+	_3yr(3.0),
+	_5yr(5.0),
+	_10yr(10.0),
+	_15yr(15.0),
+	_20yr(20.0),
+	_30yr(30.0),
+	_inflation(100.0),
+	_XCCY_basis__USD(1000.0);
+	
+	protected Double TenorDouble;
+	IRTenor(Double _TenorDouble){
+		TenorDouble=_TenorDouble;
+	}
+	public String getNom(){
+		return (this.toString()).substring(1);
+	}
+
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+
+import javax.sql.rowset.spi.XmlReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		//GIRR Test
+		XmlreaderGIRR xmlreaderGIRR = new XmlreaderGIRR();
+		System.out.println("Alpha= "+XmlreaderGIRR.alpha());
+		System.out.println("x= "+XmlreaderGIRR.GIRRx());
+		System.out.println("Thêta= "+XmlreaderGIRR.GIRRtheta());
+		System.out.println("Vega Risk Weight= "+XmlreaderGIRR.VegaRiskWeight());
+		System.out.println("Liquidity Horizon= "+XmlreaderGIRR.GIRRlh());
+		System.out.println("Gamma= "+XmlreaderGIRR.GIRRgamma());
+		Map<IRTenor,Double> Test = XmlreaderGIRR.GIRRweights();
+		
+		for(Map.Entry<IRTenor, Double> sample : Test.entrySet()){
+			System.out.println(sample.getKey()+" has a Weight of "+sample.getValue());
+		}		
+
+//	
+//		//EQUITY Test
+//		XmlreaderEQUITY xmlreaderEQUITY = new XmlreaderEQUITY();
+//		System.out.println("Large liquidity horizon= "+XmlreaderEQUITY.EQUITY_Large_lh());
+//		System.out.println("Small liquidity horizon= "+XmlreaderEQUITY.EQUITY_Small_lh());
+//		System.out.println("Gamma= "+XmlreaderEQUITY.EQUITYgamma());
+//		Map<EquityBucket,Double> Teest = XmlreaderEQUITY.Equityweights();
+//		for(Map.Entry<EquityBucket, Double> sample : Teest.entrySet()){
+//			System.out.println("Bucket"+sample.getKey()+" has a Weight of "+sample.getValue());
+//		}
+
+
+
+	}
+}
